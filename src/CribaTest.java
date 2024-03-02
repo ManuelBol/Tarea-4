f819454090805cdd967bd7479777608582b70713
ManuelBol <manbolest@alu.edu.gva.es>
2024-03-02 09:55:31 +0100

    Modificar test

--- src/CribaTest.java
+++ src/CribaTest.java
@@ -7,16 +7,15 @@ class CribaTest {
 
     @Test
     void generarPrimos() {
-        Criba a = new Criba();
 
         //Si el valor introducido es 2 o mayor (En este caso 30)
         int[] expected = {2,3,5,7,11,13,17,19,23,29};
-        int[] actual = a.generarPrimos(30);
+        int[] actual = Criba.generarPrimos(30);
         assertArrayEquals(expected, actual);
 
         //Si el valor introducido es menor que 2 (En este caso 1)
         int[] expected1 = {};
-        int[] actual1 = a.generarPrimos(1);
+        int[] actual1 = Criba.generarPrimos(1);
         assertArrayEquals(expected1, actual1);
 
 
