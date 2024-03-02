609c7a7495ae7bf183fa79ff8eec73c4090271f5
ManuelBol <manbolest@alu.edu.gva.es>
2024-03-02 12:01:13 +0100

    Extraer metodos desde el metodo generarPrimos

--- src/Criba.java
+++ src/Criba.java
@@ -6,50 +6,71 @@ public class Criba {
     public static int[] generarPrimos (int max) {
 
         if (max >= 2) {
-            // Declaraciones
-            int arrayLength = max + 1; // Tamaño del array
+            int arrayLength = max + 1;
             boolean[] esPrimo = new boolean[arrayLength];
 
-            // Inicializar el array
-            for (int i = 0; i < arrayLength; i++) {
-                esPrimo[i] = true;
-            }
+            inicializarArrayTrue(arrayLength, esPrimo);
 
-            // Eliminar el 0 y el 1, que no son primos
+            // 0 y 1 no son primos
             esPrimo[0] = false;
             esPrimo[1] = false;
 
-            // Criba
-            for (int i = 2; i < Math.sqrt(arrayLength) + 1; i++) {
-                if (esPrimo[i]) {
-                    // Eliminar los múltiplos de i
-                    for (int j = 2 * i; j < arrayLength; j += i) {
-                        esPrimo[j] = false;
-                    }
-                }
-            }
+            cribaPrimos(arrayLength, esPrimo);
 
-            // ¿Cuántos primos hay?
-            int cuenta = 0;
-            for (int i = 0; i < arrayLength; i++) {
-                if (esPrimo[i]) {
-                    cuenta++;
-                }
-            }
+            int cantidadPrimos = getCuentaPrimos(arrayLength, esPrimo);
 
-            // Rellenar el vector de números primos
-            int[] primos = new int[cuenta];
-            for (int i = 0, j = 0; i < arrayLength; i++) {
-                if (esPrimo[i]) {
-                    primos[j++] = i;
-                }
-            }
+
+            int[] primos = new int[cantidadPrimos];
+            inicializarArrayPrimos(arrayLength, esPrimo, primos);
+
             return primos;
 
         } else { // max < 2
             return new int[0];  // Vector vacío
         }
     }
+
+
+    //Inicializar el array con todos los elementos a True.
+    private static void inicializarArrayTrue(int arrayLength, boolean[] esPrimo) {
+        for (int i = 0; i < arrayLength; i++) {
+            esPrimo[i] = true;
+        }
+    }
+
+    //Cambiar a False todos los no primos.
+    private static void cribaPrimos(int arrayLength, boolean[] esPrimo) {
+        for (int i = 2; i < Math.sqrt(arrayLength) + 1; i++) {
+            if (esPrimo[i]) {
+                // Eliminar los múltiplos de i
+                for (int j = 2 * i; j < arrayLength; j += i) {
+                    esPrimo[j] = false;
+                }
+            }
+        }
+    }
+
+    //Devolver la cantidad de numeros primos.
+    private static int getCuentaPrimos(int arrayLength, boolean[] esPrimo) {
+        int cuenta = 0;
+        for (int i = 0; i < arrayLength; i++) {
+            if (esPrimo[i]) {
+                cuenta++;
+            }
+        }
+        return cuenta;
+    }
+
+    //Inicializar el array con los numeros primos
+    private static void inicializarArrayPrimos(int arrayLength, boolean[] esPrimo, int[] primos) {
+        for (int i = 0, j = 0; i < arrayLength; i++) {
+            if (esPrimo[i]) {
+                primos[j] = i;
+                j++;
+            }
+        }
+    }
+
 
     public static void main(String[] args) {
 
